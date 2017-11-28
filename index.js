4d09eeb7b0d18f20e8d29e0cde506653459c486a
Onur Eker <yonureker@gmail.com>
2017-11-28 18:14:26 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function theBeatlesPlay(musicians, instruments){
+  let myArray = [];
+  for (let i = 0; i < musicians.length; i++){
+    myArray.push(musicians[i] + " plays " + instruments[i])
+  }
+  return myArray;
+}
+
+function johnLennonFacts(){
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+}
